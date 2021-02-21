5c18c3c66976df1e9e7b84d1e798edcbee9488c1
Kuba Birecki <kuba.birecki@crystalplanet.io>
2021-02-21 20:27:11 +0100

    Remove cmake rules for JLINK

--- app/boards/arm/orion_92/board.cmake
+++ app/boards/arm/orion_92/board.cmake
@@ -2,6 +2,4 @@
 # SPDX-License-Identifier: MIT
 
 board_runner_args(nrfjprog "--nrf-family=NRF52")
-board_runner_args(jlink "--device=nrf52" "--speed=4000")
 include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
-include(${ZEPHYR_BASE}/boards/common/jlink/board.cmake)
